6dd11eef2038c976fdf6b85ed55c5921328f8fc4
Alan Mendoza <alanrodrigo.mg@gmail.com>
2023-08-14 13:02:09 -0400

    Creando funcion de asignacion al client attach y mejorando la funcion index y show para mostrar a los clientes con sus servicios asignados

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -13,7 +13,20 @@ class ClientController extends Controller
     public function index()
     {
         $clients = Client::all();
-        return response()->json($clients);
+
+        $array = [];
+        foreach ($clients as $client) {
+            $array[] = [
+                'id' => $client->id,
+                'name' => $client->name,
+                'email' => $client->email,
+                'phone' => $client->phone,
+                'address' => $client->address,
+                'services' => $client->services
+            ];
+        }
+
+        return response()->json($array);
     }
 
     /**
@@ -49,7 +62,14 @@ class ClientController extends Controller
      */
     public function show(Client $client)
     {
-        return response()->json($client);
+        $data = [
+            'message' => 'Detalles del cliente',
+            'client' => $client,
+            'services' => $client->services
+        ];
+        // return response()->json($client);
+        // return response()->json($client->services);
+        return response()->json($data);
     }
 
     /**
@@ -88,6 +108,19 @@ class ClientController extends Controller
 
         $data = [
             'message' => 'Cliente eliminado correctamente',
+            'client' => $client
+        ];
+
+        return response()->json($data);
+    }
+
+    public function attach(Request $request)
+    {
+        $client = Client::find($request->client_id);
+        $client->services()->attach($request->service_id);
+
+        $data = [
+            'message' => 'Servicio asignado correctamente',
             'client' => $client
         ];
 
