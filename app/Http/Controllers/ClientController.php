e618440cce1e71fe0fff86a3a7efd79eeb1f1a71
Alan Mendoza <alanrodrigo.mg@gmail.com>
2023-08-14 11:41:41 -0400

    Modulo finalizado de Client para funcionar como api

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -12,7 +12,8 @@ class ClientController extends Controller
      */
     public function index()
     {
-        //
+        $clients = Client::all();
+        return response()->json($clients);
     }
 
     /**
@@ -28,7 +29,19 @@ class ClientController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $client = new Client;
+        $client->name = $request->name;
+        $client->email = $request->email;
+        $client->phone = $request->phone;
+        $client->address = $request->address;
+        $client->save();
+
+        $data = [
+            'message' => 'Cliente creado correctamente',
+            'client' => $client
+        ];
+
+        return response()->json($data);
     }
 
     /**
@@ -36,7 +49,7 @@ class ClientController extends Controller
      */
     public function show(Client $client)
     {
-        //
+        return response()->json($client);
     }
 
     /**
@@ -52,7 +65,18 @@ class ClientController extends Controller
      */
     public function update(Request $request, Client $client)
     {
-        //
+        $client->name = $request->name;
+        $client->email = $request->email;
+        $client->phone = $request->phone;
+        $client->address = $request->address;
+        $client->save();
+
+        $data = [
+            'message' => 'Cliente actualizado correctamente',
+            'client' => $client
+        ];
+
+        return response()->json($data);
     }
 
     /**
@@ -60,6 +84,13 @@ class ClientController extends Controller
      */
     public function destroy(Client $client)
     {
-        //
+        $client->delete();
+
+        $data = [
+            'message' => 'Cliente eliminado correctamente',
+            'client' => $client
+        ];
+
+        return response()->json($data);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('clients', [ClientController::class, 'index']);
+Route::post('clients', [ClientController::class, 'store']);
+Route::get('clients/{client}', [ClientController::class, 'show']);
+Route::put('clients/{client}', [ClientController::class, 'update']);
+Route::delete('clients/{client}', [ClientController::class, 'destroy']);
